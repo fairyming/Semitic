d6363018b8b3a8f01a586d0cb253fe0c15896787
fairy <fairy>
2020-04-23 15:46:24 +0800

    新增情报爬虫

---
+++ crawler/api/ioc.py
@@ -0,0 +1,11 @@
+from api.Database import Database
+
+def ioc_push(ioc_list):
+    link_ioc_mongo = Database()
+    tags = ["domain", "ip", "url", "email", "hash"]
+    for ioc in ioc_list:
+        for tag in tags:
+            if tag in ioc:
+                link_ioc_mongo.insert(tag, ioc)
+                print(ioc)
+    link_ioc_mongo.close()
